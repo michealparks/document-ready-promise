1e4a97e3d63f793d2447e383035a71a43790c3fd
Micheal Parks <michealparks1989@gmail.com>
2015-11-20 15:42:36 -0500

    Ready is now a function. Will properly fit into a promise chain

--- document-ready-promise.js
+++ document-ready-promise.js
@@ -4,22 +4,23 @@
   if (typeof module !== 'undefined') module.exports = promise
   else document.ready = promise
 
-})(window.document, new Promise(function(resolve) {
+})(window.document, function() {
 
   var d = document, w = window,
       loaded = /^loaded|^i|^c/.test(d.readyState),
       DOMContentLoaded = 'DOMContentLoaded', load = 'load'
 
-  if (loaded) return resolve()
+  return new Promise(function(resolve) {
+    if (loaded) return resolve()
 
-  function onReady() {
-    resolve()
-    d.removeEventListener(DOMContentLoaded, onReady)
-    w.removeEventListener(load, onReady)
-  }
+    function onReady() {
+      resolve()
+      d.removeEventListener(DOMContentLoaded, onReady)
+      w.removeEventListener(load, onReady)
+    }
 
-  d.addEventListener(DOMContentLoaded, onReady)
-  w.addEventListener(load, onReady)
-
-}))
+    d.addEventListener(DOMContentLoaded, onReady)
+    w.addEventListener(load, onReady)
+  })
+})
 
